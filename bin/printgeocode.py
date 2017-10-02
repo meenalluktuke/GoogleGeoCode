bc07306799b223fb679af069f27ccdc7fa8479dc
meenalluktuke <meenal.luktuke@gmail.com>
2017-10-02 08:18:09 +0200

    Add files via upload
    Added new code for reverse geocoding

--- bin/printgeocode.py
+++ bin/printgeocode.py
@@ -1,50 +1,87 @@
 import splunk.Intersplunk
 from geopy import geocoders
 
- 
 def geocode(string):
-  address=string
-# API_KEY From Google, to be added in appconfig.py file
-  fname = open('../local/appsetup.conf', 'r')
-  for line in fname.readlines():
-	if '=' not in line:
-		continue
-	l = line.strip().split('=')
-	api_key_val=l[1]
+
+	address=string
+	# API_KEY From Google, to be added in appsetup.conf file
+	fname = open('../local/appsetup.conf', 'r')
+	for line in fname.readlines():
+		if '=' not in line:
+			continue
+		l = line.strip().split('=')
+		api_key_val=l[1]
+			
+	g = geocoders.GoogleV3(api_key=api_key_val)
+	place, (lat,lng) = g.geocode(address)
+
+	# Returns latitude,longitude pair
+	return lat,lng
 	
-  g = geocoders.GoogleV3(api_key=api_key_val)
-  place, (lat,lng) = g.geocode(address)
-  loc = (lat,lng)
-# Returns latitude,longitude pair
-  return loc
+
+def reverse_geocode(location):
+	Point=location
+
+	fname = open('../local/appsetup.conf', 'r')
+	for line in fname.readlines():
+		if '=' not in line:
+			continue
+		l = line.strip().split('=')
+		api_key_val=l[1]
+
+	
+	# Reverse Geocoding
+	g = geocoders.GoogleV3(api_key=api_key_val)
+	place = g.reverse(Point,exactly_one=True)
+  
+        # Returns Address field
+	return place.address
  
  # A basic shell for any custom streaming command. Just pass the events to it
 def customcommand(results, settings):
-  try:
-    fields, argvals = splunk.Intersplunk.getKeywordsAndOptions()
-# This is the field passed with command, has to be a valid address
-    address_field = fields[0]
-     # Set a default return value
-    address_value = "Field does not exist"
+	try:
+		fields, argvals = splunk.Intersplunk.getKeywordsAndOptions()
+		Type = argvals.get('type',None)
+		address = argvals.get('address',None)
+		lat = argvals.get('latfield',None)
+		lon = argvals.get('lonfield',None)
+
+		# Missing arguments validation
+		if Type == None:
+			splunk.Intersplunk.generateErrorResults("'type' argument required, such as type=geocode or type=reverse")
+			exit(0)
+		if Type == "geocode":
+			if(address == None):
+				splunk.Intersplunk.generateErrorResults("String 'address' argument required")
+				exit(0)
+		if Type == "reverse":
+			if lat == None or lon == None:
+				splunk.Intersplunk.generateErrorResults("Latitude and Longitude 'latfield,lonfield' arguments required")
+				exit(0)
+
      # If the parameter provided exists as a field in the event, run the Geocode math on its value
-    for result in results:
-       # If field exists in event
-      if address_field in result:
-         # Get the field's actual value
-        address_value = result[address_field]
-         # Create the new field we'll place into the events
-	# New field will be original fieldname appended with _geolocation
-	# For e.g. if fieldname is address, returned field will be addresS_geolocation
-        newfield = address_field + "_geolocation"
-         # Finally, run the math on the field's value and place it into the newfield we just created
-        result[newfield] = geocode(address_value)
- 
+		for result in results:
+                # Based on the type field, decide whether it's address to lat,long or reverse.
+			if Type == "geocode":
+				newfield1 = "geolocation_lat"
+				newfield2 = "geolocation_lon"
+
+				address_value = result[address]
+				lat,lng = geocode(address_value)
+				result[newfield1]=lat
+				result[newfield2]=lng
+			if Type == "reverse":
+				newfield = "geolocation_addr"
+				lat_value = result[lat]
+				lon_value = result[lon]
+				loc=lat_value,lon_value
+				result[newfield] = reverse_geocode(loc)
      # Let the modified events flow back into the search results
-    splunk.Intersplunk.outputResults(results)
+		splunk.Intersplunk.outputResults(results)
  
-  except:
-    import traceback
-    stack =  traceback.format_exc()
+	except:
+		import traceback
+		stack =  traceback.format_exc()
  
  # Get the events from splunk
 results, dummyresults, settings = splunk.Intersplunk.getOrganizedResults()
